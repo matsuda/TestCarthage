93c362eda4bc6bf190b6e010dc2c560c21b85787
matsuda <kosukematsuda@gmail.com>
2015-08-26 19:26:21 +0900

    support Carthage

---
+++ TestCarthage/Person.swift
@@ -0,0 +1,15 @@
+//
+//  Person.swift
+//  TestCarthage
+//
+//  Created by Kosuke Matsuda on 2015/08/26.
+//  Copyright © 2015年 matsuda. All rights reserved.
+//
+
+import Foundation
+
+public class Person: NSObject {
+    public func hello() {
+        print("hello!")
+    }
+}
---
+++ TestCarthage/TestCarthage.h
@@ -0,0 +1,19 @@
+//
+//  TestCarthage.h
+//  TestCarthage
+//
+//  Created by Kosuke Matsuda on 2015/08/26.
+//  Copyright © 2015年 matsuda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestCarthage.
+FOUNDATION_EXPORT double TestCarthageVersionNumber;
+
+//! Project version string for TestCarthage.
+FOUNDATION_EXPORT const unsigned char TestCarthageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestCarthage/PublicHeader.h>
+
+
